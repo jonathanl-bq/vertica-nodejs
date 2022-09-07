a1eadab1db08ff14a9382879d623e0ab7fc8a53b
Jonathan Louie <jonathanl@bitquilltech.com>
2022-09-07 09:40:37 -0700

    restore protocol version

--- packages/vertica-nodejs/lib/client.js
+++ packages/vertica-nodejs/lib/client.js
@@ -36,6 +36,7 @@ class Client extends EventEmitter {
     this.port = this.connectionParameters.port
     this.host = this.connectionParameters.host
     this.backup_server_node = this.connectionParameters.backup_server_node
+    this.protocol_version = this.connectionParameters.protocol_version
 
     // "hiding" the password so it doesn't show up in stack traces
     // or if the client is console.logged
